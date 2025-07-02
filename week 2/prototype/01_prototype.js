8d20416de823e6f395949f3ad0a02db8efa8a336
arpan-gt <arpan.main@outlook.com>
2025-07-02 15:57:10 +0530

    forEach prototype implemented

--- week 2/prototype/01_prototype.js
+++ week 2/prototype/01_prototype.js
@@ -1,5 +1,9 @@
 // * * prototypes are objects that contains all the methods properties of the datatypes like array,object,string etc.
 
+
+
+//todo ------------------------------------------------------------------------------------------------------
+
 let arr = [1, 2, 3, 4, 5, 6];
 
 // * * signature of map function
@@ -21,3 +25,51 @@ if (!Array.prototype.myMap)
 const newArray=arr.myMap((el) => el * 3);
 console.log('map function prototype');
 console.log(newArray);
+
+
+//todo -------------------------------------------------------------------------------------------------------
+
+//todo : Implement filter prototype function
+
+
+
+//**  signature for filter => returns a new array , takes userFunction, iterates on each element and if returns true then push the element to new Array.
+
+
+if(!Array.prototype.myFilter)
+{
+    Array.prototype.myFilter=function(userFunction)
+    {
+      const result=[];
+      for(let i=0;i<this.length;i++){
+        if(userFunction(this[i]))
+          {
+            result.push(this[i]);
+          };
+      };
+      return result
+    };
+};
+
+const filterArray=arr.myFilter((el) => el%3 == 0);
+console.log(filterArray);
+
+
+//todo ---------------------------------------------------------------------------------------------------
+
+
+//todo implement forEach prototype function
+
+// * * signature for forEach => iterates on each element , takes a userFunction 
+if(!Array.prototype.myForEach)
+{
+    Array.prototype.myForEach=function(userFunction)
+    {
+      for(let i=0;i<this.length;i++)
+        {
+          userFunction(this[i],i);
+        };
+    };
+};
+
+console.log();
